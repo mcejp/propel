bea5eff2b4792bf27ff6d6a9a97e5d3f43bf1d62
Martin Cejp <cejp.mart@gmail.com>
2022-08-01 20:31:09 +0200

    chicken scheme

---
+++ chicken/fib-user.scm
@@ -0,0 +1,10 @@
+;;; fib-user.scm
+(import (chicken foreign) (chicken format))
+
+#>
+  extern int fib(int n);
+<# 
+(define xfib (foreign-lambda int "fib" int))
+(do ((i 0 (+ i 1))) ((> i 10))
+  (printf "~A " (xfib i)))
+(newline)
