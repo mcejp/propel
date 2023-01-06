575e8468c6b754ac13ad49a00bb54c87fe06a593
Martin Cejp <cejp.mart@gmail.com>
2023-01-07 00:38:29 +0100

    Update expected test outputs

--- out/vga-palette/50-cpp.cpp
+++ out/vga-palette/50-cpp.cpp
@@ -3,6 +3,7 @@ inline int builtin_add_ii(int a, int b) { return a + b; }
 inline int builtin_sub_ii(int a, int b) { return a - b; }
 inline int builtin_mul_ii(int a, int b) { return a * b; }
 inline int builtin_lessthan_ii(int a, int b) { return (a < b) ? 1 : 0; }
+inline int builtin_lesseq_ii(int a, int b) { return (a <= b) ? 1 : 0; }
 inline int builtin_greaterthan_ii(int a, int b) { return (a > b) ? 1 : 0; }
 inline int builtin_and_ii(int a, int b) { return a && b; }
 inline int builtin_not_i(int a) { return a ? 0 : 1; }
