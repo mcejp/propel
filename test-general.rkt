e219bad70ce66f9bc10a8379f9b5434cdd44a054
Martin Cejp <cejp.mart@gmail.com>
2023-05-29 17:58:44 +0200

    Reformat with recent version of `fmt`
    This fixes CI failures

--- test-general.rkt
+++ test-general.rkt
@@ -5,42 +5,41 @@
          "propel-syntax.rkt"
          "scope.rkt")
 
-(test-case
- "dot.rkt"
- (define stx (parse-module "tests/dot.rkt"))
- (define mod-core-forms (resolve-forms (hash) stx))
- (define ser (serialize-module mod-core-forms #f))
- (check-equal?
-  ser
-  `((#%begin (#%defun get-name ((foo Bar)) str (#%begin (#%dot foo name))))
-    #f
-    ((,(string->path "tests/dot.rkt") 0 0 0 47)
-     (#f 0 0 0 47)
-     ((#f 0 0 0 46)
-      (#f 0 0 0 46)
-      (#f 0 7 7 8)
-      ((#f 0 9 9 11) ((#f 0 1 1 9) (#f 0 1 1 3) (#f 0 4 4 3)))
-      (#f 0 6 6 3)
-      ((#f 0 -28 -28 46)
+(test-case "dot.rkt"
+  (define stx (parse-module "tests/dot.rkt"))
+  (define mod-core-forms (resolve-forms (hash) stx))
+  (define ser (serialize-module mod-core-forms #f))
+  (check-equal?
+   ser
+   `((#%begin (#%defun get-name ((foo Bar)) str (#%begin (#%dot foo name))))
+     #f
+     ((,(string->path "tests/dot.rkt") 0 0 0 47)
+      (#f 0 0 0 47)
+      ((#f 0 0 0 46)
        (#f 0 0 0 46)
-       ((#f 1 2 34 8) (#f 0 0 0 8) (#f 0 0 0 8) (#f 0 0 0 8))))))))
+       (#f 0 7 7 8)
+       ((#f 0 9 9 11) ((#f 0 1 1 9) (#f 0 1 1 3) (#f 0 4 4 3)))
+       (#f 0 6 6 3)
+       ((#f 0 -28 -28 46)
+        (#f 0 0 0 46)
+        ((#f 1 2 34 8) (#f 0 0 0 8) (#f 0 0 0 8) (#f 0 0 0 8))))))))
 
-(test-case
- "hello.rkt"
- (define stx (parse-module "tests/hello.rkt"))
- (define mod-core-forms (resolve-forms (hash) stx))
- (define ser (serialize-module mod-core-forms #f))
- (check-equal?
-  ser
-  `((#%begin (#%defun increment ((n int)) int (#%begin (#%app + n 1))))
-    #f
-    ((,(string->path "tests/hello.rkt") 0 0 0 45)
-     (#f 0 0 0 45)
-     ((#f 0 0 0 44)
-      (#f 0 0 0 44)
-      (#f 0 7 7 9)
-      ((#f 0 10 10 9) ((#f 0 1 1 7) (#f 0 1 1 1) (#f 0 2 2 3)))
-      (#f 0 6 6 3)
-      ((#f 0 -27 -27 44)
-       (#f 0 0 0 44)
-       ((#f 1 2 33 7) (#f 0 0 0 7) (#f 0 1 1 1) (#f 0 2 2 1) (#f 0 2 2 1))))))))
+(test-case "hello.rkt"
+  (define stx (parse-module "tests/hello.rkt"))
+  (define mod-core-forms (resolve-forms (hash) stx))
+  (define ser (serialize-module mod-core-forms #f))
+  (check-equal?
+   ser
+   `((#%begin (#%defun increment ((n int)) int (#%begin (#%app + n 1))))
+     #f
+     ((,(string->path "tests/hello.rkt") 0 0 0 45)
+      (#f 0 0 0 45)
+      ((#f 0 0 0 44) (#f 0 0 0 44)
+                     (#f 0 7 7 9)
+                     ((#f 0 10 10 9) ((#f 0 1 1 7) (#f 0 1 1 1) (#f 0 2 2 3)))
+                     (#f 0 6 6 3)
+                     ((#f 0 -27 -27 44) (#f 0 0 0 44)
+                                        ((#f 1 2 33 7) (#f 0 0 0 7)
+                                                       (#f 0 1 1 1)
+                                                       (#f 0 2 2 1)
+                                                       (#f 0 2 2 1))))))))
--- test-propel-serialize.rkt
+++ test-propel-serialize.rkt
@@ -8,29 +8,26 @@
   (define stx (read-syntax "input.rkt" port))
   (check-equal? (serialize-expr stx) expectation))
 
-(test-case
- "Literal"
- (check-serialization-equal? "5" (cons 5 (srcloc "input.rkt" #f #f 1 1))))
+(test-case "Literal"
+  (check-serialization-equal? "5" (cons 5 (srcloc "input.rkt" #f #f 1 1))))
 
 (test-case "Form"
-           (check-serialization-equal?
-            "(#%print 5)"
-            (cons '(#%print 5)
-                  (list (srcloc "input.rkt" #f #f 1 11)
-                        (srcloc "input.rkt" #f #f 2 7)
-                        (srcloc "input.rkt" #f #f 10 1)))))
+  (check-serialization-equal? "(#%print 5)"
+                              (cons '(#%print 5)
+                                    (list (srcloc "input.rkt" #f #f 1 11)
+                                          (srcloc "input.rkt" #f #f 2 7)
+                                          (srcloc "input.rkt" #f #f 10 1)))))
 
 (test-case "Cons"
-           (check-serialization-equal?
-            "(foo . bar)"
-            (cons '(foo . bar)
-                  (list (srcloc "input.rkt" #f #f 2 3)
-                        (srcloc "input.rkt" #f #f 8 3)))))
+  (check-serialization-equal? "(foo . bar)"
+                              (cons '(foo . bar)
+                                    (list (srcloc "input.rkt" #f #f 2 3)
+                                          (srcloc "input.rkt" #f #f 8 3)))))
 
 (test-case "Compression"
-           (define example
-             (list (srcloc "input.rkt" #f #f 1 11)
-                   (srcloc "input.rkt" #f #f 2 7)
-                   (srcloc "input.rkt" #f #f 10 1)))
-           (check-equal? (compress-srcloc-tree example)
-                         '(("input.rkt" 0 0 0 11) (#f 0 0 1 7) (#f 0 0 8 1))))
+  (define example
+    (list (srcloc "input.rkt" #f #f 1 11)
+          (srcloc "input.rkt" #f #f 2 7)
+          (srcloc "input.rkt" #f #f 10 1)))
+  (check-equal? (compress-srcloc-tree example)
+                '(("input.rkt" 0 0 0 11) (#f 0 0 1 7) (#f 0 0 8 1))))
